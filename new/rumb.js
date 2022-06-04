c87e7ba1508f6fd32b1b3a79b753facfebfbc3f7
HaroyanSamvel <sh.samvel.haroyan@gmail.com>
2022-06-04 18:00:50 +0400

    2

--- new/rumb.js
+++ new/rumb.js
@@ -1,38 +1,40 @@
-class Rumb {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.multiply = 0
-        this.directions = [];
-        this.energy = 20
-    }
-    getNewCoordinates() {
-        this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x, this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y],
-            [this.x + 1, this.y],
-            [this.x - 1, this.y + 1],
-            [this.x, this.y + 1],
-            [this.x + 1, this.y + 1]
-        ];
-    }
-    chooseCell(character) {
-        this.getNewCoordinates()
-        var found = [];
-        for (var i in this.directions) {
-            var x = this.directions[i][0];
-            var y = this.directions[i][1];
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-                if (matrix[y][x] == character) {
-                    found.push(this.directions[i]);
-                }
-            }
-        }
+class Rumb  extends LivingCreature {
+    // constructor(x, y) {
+        constructor(x, y) {
+            super(x,y);
+    //     this.x = x;
+    //     this.y = y;
+    //     this.multiply = 0
+    //     this.directions = [];
+         this.energy = 20;
+    // }
+    // getNewCoordinates() {
+    //     this.directions = [
+    //         [this.x - 1, this.y - 1],
+    //         [this.x, this.y - 1],
+    //         [this.x + 1, this.y - 1],
+    //         [this.x - 1, this.y],
+    //         [this.x + 1, this.y],
+    //         [this.x - 1, this.y + 1],
+    //         [this.x, this.y + 1],
+    //         [this.x + 1, this.y + 1]
+    //     ];
+    // }
+    // chooseCell(character) {
+    //     this.getNewCoordinates()
+    //     var found = [];
+    //     for (var i in this.directions) {
+    //         var x = this.directions[i][0];
+    //         var y = this.directions[i][1];
+    //         if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+    //             if (matrix[y][x] == character) {
+    //                 found.push(this.directions[i]);
+    //             }
+    //         }
+    //     }
 
-        return found;
-    }
+    //     return found;
+    // }
 
     mul() {
         this.multiply++;
